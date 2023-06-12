3f893b0681448997269f136c337e842481ff35db
diya17 <diyanna.3@gmail.com>
2023-06-12 18:34:54 -0400

    [GECO-141] Adding derivedFile param to imogens completed request

--- giles-eco-imogen/src/main/java/edu/asu/diging/gilesecosystem/imogen/core/service/impl/ImageExtractionManager.java
+++ giles-eco-imogen/src/main/java/edu/asu/diging/gilesecosystem/imogen/core/service/impl/ImageExtractionManager.java
@@ -166,7 +166,7 @@ public class ImageExtractionManager extends AExtractionManager implements IImage
         completedRequest.setStatus(status);
         completedRequest.setExtractionDate(OffsetDateTime.now(ZoneId.of("UTC")).toString());
         completedRequest.setPages(pages);
-
+        completedRequest.setDerivedFile(true);
         progressManager.setPhase(ProgressPhase.DONE);
         try {
             requestProducer.sendRequest(completedRequest,
